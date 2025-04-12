caf90aa532890a3c5179af74edbb80d5526040f6
Skylar <skylar.som@gmail.com>
2025-04-12 00:41:47 -0400

    Organized build.gradle a bit

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,28 +13,45 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation("io.vavr:vavr:0.10.6")
+
+    //region Quarkus Code
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
-    implementation(enforcedPlatform("${quarkusPlatformGroupId}:quarkus-amazon-services-bom:${quarkusPlatformVersion}"))
-    implementation("io.quarkus:quarkus-hibernate-reactive")
+
     implementation("io.quarkus:quarkus-mutiny")
-    implementation("io.quarkus:quarkus-hibernate-orm")
-    implementation("io.quarkus:quarkus-rest-client-jackson")
-    implementation("io.quarkus:quarkus-hibernate-validator")
-    implementation("io.quarkiverse.amazonservices:quarkus-amazon-eventbridge")
-    implementation("io.quarkus:quarkus-amazon-lambda-rest")
-    implementation("io.quarkus:quarkus-logging-json")
-    implementation("io.quarkus:quarkus-reactive-pg-client")
+    implementation("io.quarkus:quarkus-arc")
+    implementation("io.quarkus:quarkus-cache")
+    //endregion
+
+    //region HTTP
     implementation("io.quarkus:quarkus-rest")
     implementation("io.quarkus:quarkus-rest-jackson")
-    implementation("io.quarkus:quarkus-arc")
+    implementation("io.quarkus:quarkus-rest-client-jackson")
+    implementation("io.quarkiverse.resteasy-problem:quarkus-resteasy-problem:3.20.0")
+    implementation("io.quarkus:quarkus-hibernate-validator")
+    //endregion
+
+    //region Database
+    implementation("io.quarkus:quarkus-hibernate-reactive")
+    implementation("io.quarkus:quarkus-hibernate-orm")
     implementation("io.quarkus:quarkus-jdbc-postgresql")
-    implementation("io.quarkus:quarkus-cache")
+    implementation("io.quarkus:quarkus-reactive-pg-client")
+
     implementation("io.quarkus:quarkus-flyway")
 
     implementation("org.flywaydb:flyway-database-postgresql")
-    implementation("io.vavr:vavr:0.10.6")
+    //endregion
 
+    //region AWS
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:quarkus-amazon-services-bom:${quarkusPlatformVersion}"))
+
+    implementation("io.quarkiverse.amazonservices:quarkus-amazon-eventbridge")
+    implementation("io.quarkus:quarkus-amazon-lambda-rest")
+    //endregion
+
+    //region Test
     testImplementation("io.quarkus:quarkus-junit5")
+    //endregion
 }
 
 group = "io.sommers.aiintheipaw"
@@ -48,6 +65,7 @@ java {
 tasks.withType<Test> {
     systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
 }
+
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
     options.compilerArgs.add("-parameters")
