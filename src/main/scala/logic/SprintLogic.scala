1f053ea5d49c70fe8f201242b366a1354b31ce2d
Skylar <skylar.som@gmail.com>
2025-07-05 01:43:56 -0400

    Reorganize some sprint logic

--- src/main/scala/logic/SprintLogic.scala
+++ src/main/scala/logic/SprintLogic.scala
@@ -6,10 +6,10 @@ import model.problem.{InvalidValueProblem, Problem}
 import model.sprint.SprintStatus.SignUp
 import model.sprint.{Sprint, SprintSection}
 import model.user.User
-import service.{CreateSprintEntity, CreateSprintSectionEntity, SprintService}
+import service.{CreateSprintEntity, CreateSprintSectionEntity, SprintEntity, SprintEntryEntity, SprintSectionEntity, SprintService}
 
 import org.postgresql.util.PGInterval
-import zio.{Clock, Duration, IO, ZIO}
+import zio.{Clock, Duration, IO, Task, URLayer, ZIO, ZLayer}
 
 import java.time.temporal.ChronoUnit
 import scala.language.implicitConversions
@@ -19,32 +19,33 @@ trait SprintLogic {
   def createSprint(channel: Channel, user: User, duration: Duration): IO[Problem, Sprint]
 }
 
+object SprintLogic {
+  val live: URLayer[SprintService, SprintLogic] = ZLayer.fromFunction(SprintLogicLive(_))
+}
+
 case class SprintLogicLive(
   sprintService: SprintService,
-  clock: Clock
 ) extends SprintLogic {
   override def createSprint(channel: Channel, user: User, sprintDuration: Duration): IO[Problem, Sprint] = {
     for {
-      activeSprint <- sprintService.getActiveSprintByChannelId(channel.id)
+      activeSprint <- getActiveSprintByChannelId(channel)
       _ <- ZIO.when(activeSprint.isDefined)(ZIO.fail(InvalidValueProblem("There is already an active sprint")))
       newSprint <- sprintService.createSprint(CreateSprintEntity(channel.id, user.id))
-      startTime <- clock.instant
+      startTime <- ZIO.clockWith(_.instant)
       signUpSection <- sprintService.updateSprintStatus(CreateSprintSectionEntity(newSprint.id, SignUp, sprintDuration, startTime))
     } yield Sprint(
       newSprint.id,
       channel,
       user,
       Seq(
-        SprintSection(
-          signUpSection.id,
-          signUpSection.status,
-          signUpSection.startTime.toInstant,
-          signUpSection.totalTime
-        )
+        entityToSection(signUpSection)
       ),
       Seq()
     )
   }.mapError(Problem(_))
+  
+  protected def getActiveSprintByChannelId(channel: Channel): Task[Option[(SprintEntity, Seq[SprintSectionEntity], Seq[SprintEntryEntity])]] = 
+    sprintService.getActiveSprintByChannelId(channel.id)
 
   implicit def pgIntervalToDuration(interval: PGInterval): Duration = {
     java.time.Duration.ofDays(interval.getDays)
@@ -53,4 +54,11 @@ case class SprintLogicLive(
       .plus(interval.getWholeSeconds, ChronoUnit.SECONDS)
       .plus(interval.getMicroSeconds, ChronoUnit.MICROS)
   }
+  
+  private def entityToSection(sprintSectionEntity: SprintSectionEntity): SprintSection = SprintSection(
+    sprintSectionEntity.id,
+    sprintSectionEntity.status,
+    sprintSectionEntity.startTime.toInstant,
+    sprintSectionEntity.totalTime
+  )
 }
